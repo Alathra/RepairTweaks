5685b948749d521be16123a65308a9b0f28dcd1e
Aubri Seebauer <bananacreature@gmail.com>
2023-03-30 19:23:34 -0400

    Finalize v1

--- src/com/aubrithehuman/repairtweaks/RepairTweaks.java
+++ src/com/aubrithehuman/repairtweaks/RepairTweaks.java
@@ -26,28 +26,40 @@ public class RepairTweaks extends JavaPlugin implements Listener {
 			if (repairItem != null && tool != null) {
 				if (this.getConfig().getStringList("items").contains(tool.getType().name())) {
 					if (this.getConfig().getStringList("materials").contains(repairItem.getType().name())) {
+
+						//THIS IS A PRECAUTION (fixing isaacs bug!)
 						event.getInventory().setMaximumRepairCost(1000);
+
+						//calc needed mats to repair
 						int needed = 0;
 						if (tool.getItemMeta() instanceof Damageable) {
 							needed = ((Damageable) tool.getItemMeta()).getDamage() / (tool.getType().getMaxDurability() / 4) + 1;
 							needed = Math.min(needed, 4);
 						}
 
-						System.out.println(needed);
+						/*
+						* calc cost of repair, scales logarithmically
+						* second math adds in tools complexity based on previous combines at half penalty
+						* a tool with 3 previous combines (repairCost: 7), adds 3 levels to repair costs
+						* most tools will not have this effect them, mostly swords and armor are subject to this.
+						* this equation is totally refactorable and cheapenable
+						*/
 						int cost = (int) Math.round(6.2D * Math.log(4.5D * (double) Math.min(repairItem.getAmount(), needed) - 0.5D) + 1.2D);
+						//this line can be removed to standardize it
+						cost += Math.max(((((Repairable) tool.getItemMeta()).getRepairCost() + 1) / 2) - 1, 0);
+
+						//cap it
 						if (cost <= 0) {
 							cost = 0;
 						}
-
 						if (cost >= 39) {
 							cost = 39;
-							System.out.println("NOT RIGHT");
+//							System.out.println("NOT RIGHT");
 						}
-
-						System.out.println(cost);
 						event.getInventory().setRepairCost(cost);
-						if (tool.getType() == Material.TRIDENT
-								&& repairItem.getType() == Material.PRISMARINE_CRYSTALS) {
+
+						//Trident Math Specifically
+						if (tool.getType() == Material.TRIDENT && repairItem.getType() == Material.PRISMARINE_CRYSTALS) {
 							ItemStack out = tool.clone();
 							Damageable meta = (Damageable) out.getItemMeta();
 							meta.setDamage(Math.max(0, meta.getDamage() - Math.min(needed, repairItem.getAmount())
@@ -64,10 +76,16 @@ public class RepairTweaks extends JavaPlugin implements Listener {
 				}
 			}
 		}
-
-		System.out.println("Prepared Event");
 	}
 
+	/**
+	 * Most of this just makes sure not to modify the Repair cost tag when doing a raw
+	 * item repair.
+	 *
+	 * Some is also related to trident repairing
+	 *
+	 * @param event
+	 */
 	@EventHandler
 	public void click(InventoryClickEvent event) {
 		if (event.getWhoClicked() instanceof Player) {
@@ -78,6 +96,7 @@ public class RepairTweaks extends JavaPlugin implements Listener {
 					return;
 				}
 
+				//Replace grabbed item with old repairCost tag
 				if (this.getConfig().getStringList("items").contains(item.getType().name())) {
 					ItemStack[] contents = ((AnvilInventory) event.getInventory()).getContents();
 					if (contents[0] != null && contents[1] != null
@@ -89,6 +108,23 @@ public class RepairTweaks extends JavaPlugin implements Listener {
 							repairableNew.setRepairCost(current);
 							item.setItemMeta(repairableNew);
 							event.setCurrentItem(item);
+						}
+
+						//Trident Crystals refund (it consumes the whole stack, so this calculated how much to gift the player back
+						if (contents[0].getType() == Material.TRIDENT && contents[1].getType() == Material.PRISMARINE_CRYSTALS) {
+							ItemStack crystals = contents[1].clone();
+
+							int needed = 0;
+							if (contents[0].getItemMeta() instanceof Damageable) {
+								needed = ((Damageable) contents[0].getItemMeta()).getDamage() / (contents[0].getType().getMaxDurability() / 4) + 1;
+							}
+							needed = Math.min(needed, 4);
+
+							int amount = crystals.getAmount() - needed;
+							if (amount < 0) amount = 0;
+							crystals.setAmount(amount);
+
+							event.getWhoClicked().getWorld().dropItem(event.getWhoClicked().getLocation(), crystals);
 						}
 					}
 				}
